0c52223361149d1849ac7280c302a2d0c421685a
Kevin Busch <kbusch@andculture.com>
2020-09-09 15:41:08 -0400

    Moved around stuff.  Added training video 32... external widgets

---
+++ Sitefinity.Training.Web.UI/Mvc/Controllers/SystemInfoController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Web.Mvc;
+using Telerik.Sitefinity.Mvc;
+
+namespace Sitefinity.Training.Web.UI.Mvc.Controllers
+{
+    [ControllerToolboxItem(Name = "SystemInfO", Title = "System Info", SectionName = "System")]
+    public class SystemInfoController : Controller
+    {
+        public string Message { get; set; }
+
+        public ActionResult Index()
+        {
+            ViewBag.OS = Environment.OSVersion;
+            ViewBag.Message = this.Message;
+            return View("Index");
+        }
+
+        [Route("~/json/system-info")]
+        public JsonResult ShortInfo()
+        {
+            var dynamicObject = new
+            {
+                OS = Environment.OSVersion.ToString(),
+                Message = this.Message,
+            };
+
+            return Json(dynamicObject, JsonRequestBehavior.AllowGet);
+        }
+
+        protected override void HandleUnknownAction(string actionName)
+        {
+            base.ActionInvoker.InvokeAction(this.ControllerContext, "Index");
+        }
+    }
+}
