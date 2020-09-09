ea45606094758ea9d1b0e9366ef9e12e7f1e0f0e
Kevin Busch <kbusch@andculture.com>
2020-09-09 13:46:42 -0400

    event hub video changes

---
+++ Global.asax.cs
@@ -0,0 +1,38 @@
+﻿using SitefinityWebApp.MVC.ViewModels;
+using System;
+using Telerik.Sitefinity.DynamicModules.Events;
+using Telerik.Sitefinity.Model;
+using Telerik.Sitefinity.Services;
+
+namespace SitefinityWebApp
+{
+    public class Global : System.Web.HttpApplication
+    {
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            SystemManager.ApplicationStart += new EventHandler<EventArgs>(ApplicationStartHandler);
+        }
+
+        private void ApplicationStartHandler(object sender, EventArgs e)
+        {
+            EventHub.Subscribe<IDynamicContentCreatingEvent>(evt => DynamicContentCreatingEventHandler(evt));
+        }
+
+        private void DynamicContentCreatingEventHandler(IDynamicContentCreatingEvent evt)
+        {
+            var userId = evt.UserId;
+            var dynamicItem = evt.Item;
+            //var officeModel = new OfficeModel();
+
+            //if (dynamicItem.GetType().Equals(officeModel.OfficeType))
+            //{
+            //    dynamicItem.SetString("Info", "");
+            //}
+        }
+
+        protected void Application_End(object sender, EventArgs e)
+        {
+            EventHub.Unsubscribe<IDynamicContentCreatingEvent>(DynamicContentCreatingEventHandler);
+        }
+    }
+}
